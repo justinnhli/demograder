6d03851a3a5c17a05ec544bf443e330ae67843ca
Justin Li <justinnhli@gmail.com>
2015-12-20 18:09:48 -0800

    unify context building

--- demograder/views.py
+++ demograder/views.py
@@ -7,31 +7,36 @@ from .forms import FileUploadForm
 from .models import Course, Project, Submission, Student, Upload
 from .dispatcher import dispatch_submission
 
-# FIXME this should become a hierarchy of views
-# Student > Course > Project
-# so they can call each other's get_context_data()
-
-def get_student_context(**kwargs):
-    # FIXME get logged in student
+def get_context(**kwargs):
     context = {}
+    # FIXME get logged in student or redirect to log in
     context['student'] = Student.objects.get(email='justinnhli@oxy.edu')
+    if 'submission_id' in kwargs:
+        context['submission'] = Submission.objects.get(id=kwargs['submission_id'])
+    if 'submission' in context:
+        context['project'] = context['submission'].project
+    elif 'project_id' in kwargs:
+        context['project'] = Project.objects.get(id=kwargs['project_id'])
+    if 'project' in context:
+        context['course'] = context['project'].course
+    elif 'course_id' in kwargs:
+        context['course'] = Course.objects.get(id=kwargs['course_id'])
+    # FIXME check permissions or redirect to home page
     return context
 
 def index_view(request, **kwargs):
-    context = get_student_context(**kwargs)
+    context = get_context(**kwargs)
     return render(request, 'demograder/index.html', context)
 
 def course_view(request, **kwargs):
-    context = get_student_context(**kwargs)
-    context['course'] = Course.objects.get(id=kwargs['course_id'])
+    context = get_context(**kwargs)
     return render(request, 'demograder/course.html', context)
 
 def project_view(request, **kwargs):
-    context = get_student_context(**kwargs)
-    context['project'] = Project.objects.get(id=kwargs['project_id'])
+    context = get_context(**kwargs)
     if Submission.objects.filter(project=context['project'],student=context['student']).exists():
         context['submissions'] = Submission.objects.filter(project=context['project'],student=context['student']).order_by('-timestamp')
-        context['submission'] = Submission.objects.filter(project=context['project'],student=context['student']).order_by('timestamp').latest('timestamp')
+        context['submission'] = Submission.objects.filter(project=context['project'],student=context['student']).latest('timestamp')
     else:
         context['submissions'] = []
         context['submission'] = None
@@ -39,18 +44,11 @@ def project_view(request, **kwargs):
     return render(request, 'demograder/project.html', context)
 
 def project_upload_view(request, **kwargs):
-    project = Project.objects.get(id=kwargs['project_id'])
-    course = project.course
-    # FIXME authenticate somehow
-    student = Student.objects.get(email='justinnhli@oxy.edu')
-    # FIXME check student is in course
     # Collect context
-    context = get_student_context(**kwargs)
+    context = get_context(**kwargs)
     context.update(kwargs)
     context['form'] = FileUploadForm()
     # TODO load student's previous test results
-    context['course'] = course
-    context['project'] = project
     # Render list page with the documents and the form
     return render_to_response(
         'demograder/project_upload.html',
@@ -61,17 +59,12 @@ def project_upload_view(request, **kwargs):
 def project_submit_handler(request, **kwargs):
     if request.method != 'POST':
         return HttpResponseRedirect(reverse('project', kwargs=kwargs))
-    project = Project.objects.get(id=kwargs['project_id'])
-    course = project.course
-    # FIXME authenticate somehow
-    student = Student.objects.get(email='justinnhli@oxy.edu')
-    # FIXME check student is in course
     # Handle file upload
     form = FileUploadForm(request.POST, request.FILES)
     if form.is_valid():
         submission = Submission(
-                project=project,
-                student=student,
+                project=context['project'],
+                student=context['student'],
         )
         submission.save()
         # TODO handle multiple files per submission
@@ -81,15 +74,11 @@ def project_submit_handler(request, **kwargs):
         )
         upload.save()
         # find combination of all dependent files and submission and submit to RQ
-        dispatch_submission(student, project, submission)
-        return HttpResponseRedirect(reverse('project', kwargs=kwargs))
+        dispatch_submission(context['student'], context['project'], submission)
     return HttpResponseRedirect(reverse('project', kwargs=kwargs))
 
 def submission_view(request, **kwargs):
-    context = get_student_context(**kwargs)
-    context['submission'] = Submission.objects.get(id=kwargs['submission_id'])
-    context['project'] = context['submission'].project
-    if context['submission'] == Submission.objects.filter(student=context['student']).order_by('timestamp').latest('timestamp'):
+    if context['submission'] == Submission.objects.filter(student=context['student']).latest('timestamp'):
         return HttpResponseRedirect(reverse('project', kwargs={'project_id':context['project'].id}))
     context['submissions'] = Submission.objects.filter(student=context['student']).order_by('-timestamp')
     context['most_recent'] = False
