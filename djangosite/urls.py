9b4a4caf09d5372a6dd5a1ce2fe703f81ff35ea2
Justin Li <justinnhli@gmail.com>
2016-01-26 15:53:52 -0800

    remove password urls

--- djangosite/urls.py
+++ djangosite/urls.py
@@ -1,18 +1,3 @@
-"""djangosite URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
-"""
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -26,7 +11,5 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', auth_views.login, name='login'),
     url(r'^accounts/logout/$', auth_views.logout, {'next_page': '/demograder/'}, name='logout'),
-    url(r'^accounts/password_change/$', auth_views.login, name='password_change'),
-    url(r'^accounts/password_change/done$', auth_views.login, name='password_change_done'),
 ]
 urlpatterns += staticfiles_urlpatterns()
