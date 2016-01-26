236028989f1370eed2577bde217aab5b4f286477
Justin Li <justinnhli@gmail.com>
2016-01-26 15:54:05 -0800

    fix django settings

--- djangosite/settings.py
+++ djangosite/settings.py
@@ -110,8 +110,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+STATIC_URL = 'https://autograder.cc.oxy.edu/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Security (from `manage.py check --deploy`)
 X_FRAME_OPTIONS = 'DENY'
