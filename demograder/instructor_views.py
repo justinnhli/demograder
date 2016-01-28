3ca2bb4d1aae21742321f21a25fde55a35955ce2
Justin Li <justinnhli@gmail.com>
2016-01-28 12:38:52 -0800

    fix typos

--- demograder/instructor_views.py
+++ demograder/instructor_views.py
@@ -1,30 +1,23 @@
 from collections import namedtuple
-from mimetypes import guess_type
-from os.path import basename, getsize
 
 from django.contrib.auth.decorators import login_required
-from django.core.exceptions import PermissionDenied
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect, HttpResponse, Http404
-from django.shortcuts import render, render_to_response, get_object_or_404
-from django.template import RequestContext
+from django.http import HttpResponseRedirect, Http404
 
-from .forms import FileUploadForm
-from .models import Course, Enrollment, Project, Submission, Upload, Result, StudentDependency
-from .dispatcher import dispatch_submission
+from .models import Submission
 from .views import get_context
 
 @login_required
 def project_grade_view(request, **kwargs):
-    Grade = namedTuple('Grade', ('name', 'timestamp', 'score', 'uploads'))
+    Grade = namedtuple('Grade', ('name', 'timestamp', 'score', 'uploads'))
     context = get_context(request, **kwargs)
     if not context['user'].is_superuser:
         raise Http404
     submissions = []
-    for enrollment in course.enrollment_set.all():
+    for enrollment in context['course'].enrollment_set.all():
         student = enrollment.student
         name = '{} {}'.format(student.user.first_name, student.user.last_name)
-        submission = Submission.objects.filter(student=student, project=project).latest('timestamp')
+        submission = Submission.objects.filter(student=student, project=context['project']).latest('timestamp')
         if bool(submission):
             timestamp = submission.isoformat()
             score = '{}/{}'.format(submission.score, submission.max_score)
@@ -32,7 +25,7 @@ def project_grade_view(request, **kwargs):
         else:
             timestamp = 'N/A'
             score = 'N/A'
-            uploads = 'N/A'
+            uploads = []
         submissions.append(Grade(name, timestamp, score, uploads))
     context['submissions'] = sorted(submissions, key=(lambda g: g.name))
     return HttpResponseRedirect(reverse('index', kwargs=kwargs))
