4f826d05181e4676afab9989b73d80d738cba000
Justin Li <justinnhli@gmail.com>
2016-02-01 14:25:30 -0800

    allow for missing submissions in instructor view

--- demograder/instructor_views.py
+++ demograder/instructor_views.py
@@ -7,6 +7,8 @@ from django.shortcuts import render
 from .models import Submission
 from .views import get_context
 
+SubmissionDisplay = namedtuple('SubmissionDisplay', ('id', 'student', 'project', 'isoformat', 'score', 'max_score'))
+
 @login_required
 def instructor_student_view(request, **kwargs):
     context = get_context(request, **kwargs)
@@ -14,8 +16,13 @@ def instructor_student_view(request, **kwargs):
         raise Http404
     context['grades'] = []
     for course in context['student'].enrolled_course_set.all():
-        for project in course.project_set().all();
-            context['grades'].append(Submission.objects.filter(student=student, project=project).latest('timestamp'))
+        for project in course.project_set.all():
+            if not project.hidden:
+                try:
+                    submission = Submission.objects.filter(student=context['student'], project=project).latest('timestamp')
+                except Submission.DoesNotExist:
+                    submission = SubmissionDisplay(0, context['student'], project, 'N/A', 'N', 'A')
+                context['grades'].append(submission)
     context['grades'] = sorted(context['grades'], key=(lambda s: s.project.name))
     context['submissions'] = context['student'].submission_set.order_by('-timestamp')
     return render(request, 'demograder/instructor/student.html', context)
@@ -25,20 +32,12 @@ def instructor_project_view(request, **kwargs):
     context = get_context(request, **kwargs)
     if not context['user'].is_superuser:
         raise Http404
-    SubmissionDisplay = namedtuple('SubmissionDisplay', ('id', 'student', 'isoformat', 'score', 'max_score'))
     submissions = []
     for student in context['course'].student_set.all():
         try:
             submission = Submission.objects.filter(student=student, project=context['project']).latest('timestamp')
-            sid = submission.id
-            timestamp = submission.isoformat
-            score = submission.score
-            max_score = submission.max_score
         except Submission.DoesNotExist:
-            sid = 0
-            timestamp = 'N/A'
-            score = 'N'
-            max_score = 'A'
-        submissions.append(SubmissionDisplay(sid, student, timestamp, score, max_score))
+            submission = SubmissionDisplay(0, student, context['project'], 'N/A', 'N', 'A')
+        submissions.append(submission)
     context['submissions'] = sorted(submissions, key=(lambda s: s.student.name))
     return render(request, 'demograder/instructor/project.html', context)
