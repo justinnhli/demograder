35a2544a2f6030e9e87df15292f6f25e7baa90e1
Justin Li <justinnhli@gmail.com>
2016-01-31 22:29:58 -0800

    add project grades to instructor page

--- demograder/instructor_views.py
+++ demograder/instructor_views.py
@@ -12,6 +12,11 @@ def instructor_student_view(request, **kwargs):
     context = get_context(request, **kwargs)
     if not context['user'].is_superuser:
         raise Http404
+    context['grades'] = []
+    for course in context['student'].enrolled_course_set.all():
+        for project in course.project_set().all();
+            context['grades'].append(Submission.objects.filter(student=student, project=project).latest('timestamp'))
+    context['grades'] = sorted(context['grades'], key=(lambda s: s.project.name))
     context['submissions'] = context['student'].submission_set.order_by('-timestamp')
     return render(request, 'demograder/instructor/student.html', context)
 
@@ -35,5 +40,5 @@ def instructor_project_view(request, **kwargs):
             score = 'N'
             max_score = 'A'
         submissions.append(SubmissionDisplay(sid, student, timestamp, score, max_score))
-    context['submissions'] = sorted(submissions, key=(lambda g: g.student.name))
+    context['submissions'] = sorted(submissions, key=(lambda s: s.student.name))
     return render(request, 'demograder/instructor/project.html', context)
