d1acd58cdd68729fce02c4e9e7436b505bd23515
Justin Li <justinnhli@gmail.com>
2016-02-14 01:48:37 -0800

    show id in admin pages

--- demograder/admin.py
+++ demograder/admin.py
@@ -17,40 +17,41 @@ class DepartmentAdmin(admin.ModelAdmin):
 admin.site.register(Department, DepartmentAdmin)
 
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ('semester', 'catalog_id', 'title', 'students')
+    list_display = ('id', 'semester', 'catalog_id', 'title', 'students')
 admin.site.register(Course, CourseAdmin)
 
-admin.site.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'full_name')
+admin.site.register(Person, PersonAdmin)
 
 class EnrollmentAdmin(admin.ModelAdmin):
-    list_display = ('semester', 'course', 'student')
+    list_display = ('id', 'semester', 'course', 'student')
 admin.site.register(Enrollment, EnrollmentAdmin)
 
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('course', 'assignment', 'name', 'hidden')
+    list_display = ('id', 'course', 'assignment', 'name', 'hidden')
 admin.site.register(Project, ProjectAdmin)
 
 class SubmissionAdmin(admin.ModelAdmin):
-    list_display = ('project', 'student', 'timestamp', 'uploads_str')
+    list_display = ('id', 'project', 'student', 'timestamp', 'uploads_str')
 admin.site.register(Submission, SubmissionAdmin)
 
 class UploadAdmin(admin.ModelAdmin):
-    list_display = ('timestamp', 'project', 'student', 'filename')
+    list_display = ('id', 'timestamp', 'project', 'student', 'filename')
 admin.site.register(Upload, UploadAdmin)
 
 class ResultAdmin(admin.ModelAdmin):
-    list_display = (
-        'submission_timestamp',
-        'project',
-        'student',
-        'return_code',
-    )
+    list_display = ('id', 'submission_timestamp', 'project', 'student', 'return_code')
 admin.site.register(Result, ResultAdmin)
 
-admin.site.register(ProjectDependency)
+class ProjectDependencyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'producer', 'project')
+admin.site.register(ProjectDependency, ProjectDependencyAdmin)
 
 class StudentDependencyAdmin(admin.ModelAdmin):
-    list_display = ('project', 'student', 'producer')
+    list_display = ('id', 'project', 'producer', 'student')
 admin.site.register(StudentDependency, StudentDependencyAdmin)
 
-admin.site.register(ResultDependency)
+class ResultDependencyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'result__student', 'producer', 'result')
+admin.site.register(ResultDependency, ResultDependency)
