b17142181f79234581c2e4fbf2dbb5514a744405
Justin Li <justinnhli@gmail.com>
2016-08-22 17:30:11 -0700

    update server URL

--- djangosite/settings.py
+++ djangosite/settings.py
@@ -136,5 +136,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = 'https://autograder.cc.oxy.edu/static/'
+STATIC_URL = 'http://autograder.cc.oxy.edu/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
