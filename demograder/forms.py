73194603609bf03578d264b9c89e79a942c411cb
Justin Li <justinnhli@gmail.com>
2017-10-06 18:42:51 -0700

    create and use forms for multiple uploads

--- demograder/forms.py
+++ demograder/forms.py
@@ -1,5 +1,14 @@
 from django import forms
 
 
-class FileUploadForm(forms.Form):
-    file = forms.FileField()
+class SubmissionUploadForm(forms.Form):
+
+    def __init__(self, *args, **kwargs):
+        has_filenames = ('filenames' in kwargs)
+        if has_filenames:
+            filenames = kwargs.pop('filenames')
+        super().__init__(*args, **kwargs)
+        if has_filenames:
+            for i, filename in enumerate(filenames):
+                file_field = 'file_{}'.format(i)
+                self.fields[file_field] = forms.FileField(label=filename)
