618fcbaa677573c7c25748c2081b86422a1d5dd5
Justin Li <justinnhli@gmail.com>
2017-10-07 15:14:08 -0700

    always provide project to SubmissionUploadForm

--- demograder/forms.py
+++ demograder/forms.py
@@ -4,7 +4,6 @@ from django import forms
 class SubmissionUploadForm(forms.Form):
 
     def __init__(self, *args, **kwargs):
-        assert 'project' in kwargs
         project = kwargs.pop('project')
         super().__init__(*args, **kwargs)
         for file_field, filename in zip(project.file_fields, project.filenames):
--- demograder/views.py
+++ demograder/views.py
@@ -157,7 +157,7 @@ def project_submit_handler(request, **kwargs):
         return HttpResponseRedirect(reverse('project', kwargs=kwargs))
     if context['person'].may_submit(context['project']) != 'yes':
         return HttpResponseRedirect(reverse('project', kwargs=kwargs))
-    form = SubmissionUploadForm(request.POST, request.FILES)
+    form = SubmissionUploadForm(request.POST, request.FILES, project=context['project'])
     if form.is_valid():
         submission = Submission(
             project=context['project'],
