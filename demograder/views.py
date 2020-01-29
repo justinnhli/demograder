justinnhli/demograder

commit c99af57f6523f2f9b67f58ab090a9c1b94d98e4b

allow partial submissions (without all files)

demograder/views.py
@@ -164,12 +164,12 @@ def project_submit_handler(request, **kwargs):
         # TODO handle multiple files per submission
         project_files = context['project'].files
         for file_field, project_file in zip(context['project'].file_fields, context['project'].files):
-            upload = Upload(
-                submission=submission,
-                project_file=project_file,
-                file=request.FILES[file_field],
-            )
-            upload.save()
+            if field_field in request.FILES:
+                Upload(
+                    submission=submission,
+                    project_file=project_file,
+                    file=request.FILES[file_field],
+                ).save()
         enqueue_submission_dispatch(submission.id)
     return HttpResponseRedirect(reverse('project', kwargs=kwargs))
 

demograder/forms.py
@@ -7,4 +7,4 @@ class SubmissionUploadForm(forms.Form):
         project = kwargs.pop('project')
         super().__init__(*args, **kwargs)
         for file_field, filename in zip(project.file_fields, project.filenames):
-            self.fields[file_field] = forms.FileField(label=filename)
+            self.fields[file_field] = forms.FileField(label=filename, required=False)

commit 8da115be23213f03964c040917503a1706e415b2

fix partial submissions
error introduced in c99af57f6523f2f9b67f58ab090a9c1b94d98e4b

demograder/views.py
@@ -164,7 +164,7 @@ def project_submit_handler(request, **kwargs):
         # TODO handle multiple files per submission
         project_files = context['project'].files
         for file_field, project_file in zip(context['project'].file_fields, context['project'].files):
-            if field_field in request.FILES:
+            if file_field in request.FILES:
                 Upload(
                     submission=submission,
                     project_file=project_file,